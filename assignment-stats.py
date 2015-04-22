82f4d1f07b261a63635e329d1bec2761a4cc7254
Ben Nizette <benjamin.nizette@anu.edu.au>
2015-04-22 12:19:20 +1000

    Stats: Add histogram plot

--- assignment-stats.py
+++ assignment-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import numpy
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
 	print "One argument, the score sheet."
@@ -30,8 +31,40 @@ class Assignment:
 	def max(self):
 		return max(self._data)
 
+	def median(self):
+		return numpy.median(self._data)
+
+	def grades(self):
+		f = filter(lambda x: x < 50, self._data)
+		p = filter(lambda x: x < 60 and x >= 50, self._data)
+		c = filter(lambda x: x < 70 and x >= 60, self._data)
+		d = filter(lambda x: x < 80 and x >= 70, self._data)
+		hd = filter(lambda x: x > 80, self._data)
+
+		return (len(f), len(p), len(c), len(d), len(hd))
+
+	def within_std(self):
+		return len(filter(
+			lambda x: x > self.mean() - self.stdev() and x < self.mean() + self.stdev(),
+			self._data))
+
+	def above_std(self, stds=1):
+		return len(filter(
+			lambda x: x > self.mean() + stds * self.stdev(),
+			self._data))
+
+	def below_std(self, stds=1):
+		return len(filter(
+			lambda x: x < self.mean() - stds * self.stdev(),
+			self._data))
+
 	def valid(self):
 		return len(self._data) > 0
+
+	def show_histogram(self):
+		plt.hist(self._data)
+		plt.title(self.name)
+		plt.show()
 
 assignments = []
 
@@ -54,13 +87,16 @@ with open(sys.argv[1], 'r') as f:
 				pass
 
 for a in assignments:
-	if not a.valid():
+	if not a.valid() or 'course total' in a.name.lower():
 		continue
 
 	print ''
 	print '---'
 	print a.name
-	print '{:.2f} +- {:.2f}'.format(a.mean(), a.stdev())
+	print '{:.2f} +- {:.2f}  ({:.2f})'.format(a.mean(), a.stdev(), a.median())
 	print '{:.2f} - {:.2f}'.format(a.min(), a.max())
-	
+	print a.grades()
+	print '{} {} ({}) {} {}'.format(a.below_std(2), a.below_std(), a.within_std(), a.above_std(), a.above_std(2))
+	a.show_histogram()
+
 print ''
